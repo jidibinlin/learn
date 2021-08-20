e0b1e961969e17bb00fe3be0a57045f2432ee775
jidibinlin <2694273649@qq.com>
2021-08-20 14:20:32 +0800

    Longest Palindromic Substring

--- leetcode/c++/.ccls-cache/@home@qibin@learn@leetcode@c++/Longest-Palindromic-Substring.cpp
+++ leetcode/c++/.ccls-cache/@home@qibin@learn@leetcode@c++/Longest-Palindromic-Substring.cpp
@@ -4,36 +4,44 @@
 #include <string>
 using namespace std;
 
-
-int main() {
 class Solution {
 public:
     string longestPalindrome(string s) {
-        vector <vector<int>> dp(s.length(),vector<int>(s.length(),0));
-        // 当只有一个字符的时候，肯定是回文字符串
+        vector <vector<bool>> dp(s.length(),vector<bool>(s.length(),0));
         for (int i = 1; i < s.length(); ++i) {
-            dp[i][i]=1;
+            dp[i][i]=true;
         }
         int longest = 1;
-        int result[]={0,0};
+        int start=0;
 
         for (int i=2; i <= s.length(); ++i) {
-            for (int j=0 ; j+i<s.length(); j+=i) {
+            for (int j=0 ; j+i<=s.length(); ++j) {
                 if(i==2){
-                    dp[j][j+1]=s[j]==s[j+1];
+                    dp[j][j+1]=(s[j]==s[j+1]);
+                    if(longest <=2 && dp[j][j+1]){
+                        longest = i;
+                        start=j;
+                    }
+                    continue;
                 }
-                dp[j][j+i-1]=dp[j+1][j+i-2] && s[j]==s[j+i-1];
+
+                dp[j][j+i-1]=dp[j+1][j+i-2] && (s[j]==s[j+i-1]);
 
                 if(dp[j][j+i-1] && i>=longest){
-                    result[0]=j;
-                    result[1]=j+i-1;
+                    longest = i;
+                    start=j;
                 }
             }
-
         }
 
-        cout<< s.substr(result[0], result[1]);
+        cout << longest<< endl;
+
+        return s.substr(start, longest);
     }
 };
-return 0;
+
+int main() {
+    Solution *s = new Solution();
+    cout << s->longestPalindrome("bb") <<endl;
+
 }
